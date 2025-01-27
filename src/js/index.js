ae2d6e17e391d31805c42994dea65723c58da4a2
FelipePellizzer <pellizzerff@gmail.com>
2025-01-26 23:19:15 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,25 @@ const btnVoltar = document.getElementById("btn-voltar")
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
+//Virar carta
+cartoes.forEach(cartao => {
+cartao.addEventListener("click", function() {
+  const cartaVirada = cartao.querySelector(".carta-virada");
+  
+  cartao.classList.toggle("virar");
+
+  cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+const descricao = cartao.querySelector(".descricao");
+descricao.classList.toggle("esconder");
+})
+});
+
+
+
+
 //Avancar
+
 
 btnAvancar.addEventListener("click", function () {
   if (cartaoAtual === cartoes.length - 1) return;
